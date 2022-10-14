f4a5dcef93e84a06d214c1840dbb95c9cefaec58
JUAN FELIPE SANCHEZ GUAJARDO <JUAN.SANCHEZGU@correoaiep.cl>
2022-10-14 17:50:00 -0300

    cambio de puerto

--- dist/classes/server.js
+++ dist/classes/server.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 class Server {
     constructor() {
-        this.port = 5000;
+        this.port = process.env.PORT || 3000;
         this.app = (0, express_1.default)();
     }
     Start(callback) {
